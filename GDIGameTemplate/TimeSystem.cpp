5c8c9b9f8712761e62764287e29e3688dab32f36
LeeDongWon <ldw9981@gmail.com>
2024-04-24 01:23:32 +0900

    설명 변경

--- GDIGameTemplate/TimeSystem.cpp
+++ GDIGameTemplate/TimeSystem.cpp
@@ -11,15 +11,15 @@ namespace Time
 
 	void InitTime()
 	{
-		QueryPerformanceFrequency(&frequency);	// 고성능 타이머의 1초당 증가한 횟수를 가져옴
-		QueryPerformanceCounter(&prevCounter);  // 초기 카운터 값 가져옴
+		QueryPerformanceFrequency(&frequency);	// 고성능 타이머가 1초 동안 증가시킬수 있는 TickCount 값
+		QueryPerformanceCounter(&prevCounter);  // 초기 TickCount 값
 	}
 
 	void UpdateTime()
 	{
-		QueryPerformanceCounter(&currentCounter); // 현재 카운터 값 가져옴
+		QueryPerformanceCounter(&currentCounter); // 현재 TickCount 값
 		deltaTime = static_cast<float>(currentCounter.QuadPart - prevCounter.QuadPart) /
-			static_cast<float>(frequency.QuadPart);  // 카운터차이를 초단위로 변환
+			static_cast<float>(frequency.QuadPart);  // 카운터차이를 시간 초단위로 변환
 
 		prevCounter = currentCounter;
 	}	
