ad18109e8a8461db25b993c972b8f1924ef0faa9
LeeDongWon <ldw9981@gmail.com>
2024-06-04 17:06:38 +0900

    Game 컨텐츠 관련 코드 분리

--- GDIGameTemplate/WinMain.cpp
+++ GDIGameTemplate/WinMain.cpp
@@ -1,28 +1,10 @@
 #include "stdafx.h"
-#include "InputSystem.h"
-#include "RenderSystem.h"
-#include "TimeSystem.h"
-#include "Object.h"
-#include "AnimationResource.h"
+#include "Game.h"
 
-
-#define MAX_ENEMY 10
-
-Object g_player;
-Object g_enemy[MAX_ENEMY];
-
-AnimationResource* g_PlayerAnim;
-
-
-bool g_Mirror = false;
 // 비주얼 스튜디오가 만든 템플릿은 다른 추가적인 내용이 많아 이해하기 어려워 가장 간단하게 작성함.
 
 // 윈도우 프로시저 함수 선언
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-void UpdateGame();
-void RenderGame();
-void LoadResource();
-void ReleaseResource();
 
 // 프로젝트 속성 -> 링커 -> 시스템 -> 하위 시스템 -> Windows로 변경
 // 진입점 함수 정의
@@ -80,20 +62,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		freopen_s(&_tempFile, "CONOUT$", "w", stdout);
 	}	
 
-	Render::InitRender(hwnd, clientSize.cx, clientSize.cy);
-	Input::InitInput(hwnd, clientSize.cx, clientSize.cy);
-	Time::InitTime();
-	LoadResource();
-
-	g_player.Init(true);
-	g_player.m_pAnimationResource = g_PlayerAnim;
-	g_player.SetMotion(0);
 	
 
-	for (int i = 0; i < MAX_ENEMY; i++)
-	{
-		g_enemy[i].Init(false);
-	}
+	Game::Init(hwnd, clientSize.cx, clientSize.cy);
 
 	// 대기가 없는 메세지 루프
 	MSG msg;
@@ -111,15 +82,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		}
 		else
 		{
-			// 게임 루프					
-			UpdateGame();
-			RenderGame();
+			// 게임 루프				
+			Game::Update();
+			Game::Render();
 		}
 	}
 
-	ReleaseResource();
-	Input::ReleaseInput();
-	Render::Release();
+	Game::Uninit();
+	
 	if (bUseConsole)
 	{
 		FreeConsole();
@@ -140,100 +110,4 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 	return 0;
-}
-
-void UpdateGame()
-{
-	Time::UpdateTime();
-	float deltaTime = Time::GetDeltaTime();
-	Input::Update();
-
-
-	g_player.m_input.Set(0.0f,0.0f);
-	if (Input::IsCurrDn(VK_LEFT))
-	{
-		g_player.m_input.x = -1;
-	}
-	else if (Input::IsCurrDn(VK_RIGHT))
-	{
-		g_player.m_input.x = 1;
-	}
-	if (Input::IsCurrDn(VK_UP))
-	{
-		g_player.m_input.y = -1;
-	}
-	else if (Input::IsCurrDn(VK_DOWN))
-	{
-		g_player.m_input.y = 1;
-	}
-	if (Input::IsTurnDn('O'))
-	{
-		std::cout << g_player.m_posX << ' ' << g_player.m_posY << std::endl;
-	}
-	if (Input::IsCurrDn(VK_LBUTTON))
-	{
-		POINT pt = Input::GetMouseClient();
-		std::cout << pt.x << ' ' << pt.y << std::endl;
-	}
-	if (Input::IsTurnDn('T'))
-	{
-		std::cout << deltaTime << std::endl;
-	}
-	if (Input::IsTurnDn(VK_SPACE))
-	{
-		g_player.ChangeStatus(ObjectStatus::OBJECT_STATUS_ATTACK);
-	}
-	
-	if (Input::IsTurnDn('1'))
-	{
-		Render::SetColorScale(1.0f, 1.0f, 1.0f,0.1f);
-	}
-
-
-
-	// 위치 갱신
-	g_player.Update(deltaTime);
-	for (int i = 0; i < MAX_ENEMY; i++)
-	{
-		g_enemy[i].Update(deltaTime);
-	}
-
-	//충돌확인
-	for (int i = 0; i < MAX_ENEMY; i++)
-	{
-		if (g_enemy[i].m_isDead == false)
-		{
-			if (g_player.Collide(g_enemy[i]))
-			{
-				g_enemy[i].m_isDead = true;
-			}			
-		}
-	}		
-}
-void RenderGame()
-{
-	Render::BeginDraw();
-	
-	g_player.Render();
-	for (int i = 0; i < MAX_ENEMY; i++)
-	{
-		g_enemy[i].Render();
-	}
-	Render::EndDraw();
-}
-
-void LoadResource()
-{
-	g_PlayerAnim = new AnimationResource;
-
-	g_PlayerAnim->LoadAnimImage(L"../Resource/Ken.png");
-	g_PlayerAnim->LoadAnimMotion(L"../Resource/KenIdle.txt");				 //	OBJECT_STATUS_IDLE,
-	g_PlayerAnim->LoadAnimMotion(L"../Resource/KenMove.txt");				 //	OBJECT_STATUS_MOVE,
-	g_PlayerAnim->LoadAnimMotion(L"../Resource/KenAttack.txt", false);		 //	OBJECT_STATUS_ATTACK
-
-}
-
-void ReleaseResource()
-{
-	delete g_PlayerAnim;
 }
